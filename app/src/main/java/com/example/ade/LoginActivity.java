ff3c023d19b2a4542d69986aae5d224fa47e536a
farouk02 <faroukious0@gmail.com>
2021-05-06 18:24:08 +0100

    Login UI

--- app/src/main/java/com/example/ade/LoginActivity.java
+++ app/src/main/java/com/example/ade/LoginActivity.java
@@ -2,13 +2,48 @@ package com.example.ade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         setContentView(R.layout.activity_login);
+    }
+
+    public void onRegisterClick(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+    }
+
+    public void gotoFb(View view) {
+        String FACEBOOK_URL = "https://www.facebook.com/EPIC.ADE.CHLEF";
+        String FACEBOOK_PAGE_ID = "EPIC.ADE.CHLEF";
+        String uri;
+
+        try {
+            int versionCode = this.getPackageManager().getPackageInfo("com.facebook.katana", 0).versionCode;
+
+            if (versionCode >= 3002850) uri = "fb://facewebmodal/f?href=" + FACEBOOK_URL;
+            else uri = "fb://page/?id=" + FACEBOOK_PAGE_ID;
+
+        } catch (PackageManager.NameNotFoundException e) {
+            uri = FACEBOOK_URL;
+        }
+
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(uri)));
+    }
+
+    public void gotoSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ade.dz/e-paiement/")));
     }
 }
