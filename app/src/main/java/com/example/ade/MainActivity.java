d2ce46a0dcbdb29b881c914cc0342d5c00000bd1
farouk02 <faroukious0@gmail.com>
2021-05-06 18:34:33 +0100

    Login UI comleted

--- app/src/main/java/com/example/ade/MainActivity.java
+++ app/src/main/java/com/example/ade/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.ade;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        //setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, LoginActivity.class));
+
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
     }
 }
