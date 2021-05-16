26c0037d20f694edcd702514a40383a060c7d42e
farouk02 <faroukious0@gmail.com>
2021-05-16 01:10:44 +0100

    config

--- app/src/main/java/com/example/ade/Initialize.java
+++ app/src/main/java/com/example/ade/Initialize.java
@@ -1,11 +1,13 @@
 package com.example.ade;
 
+
 public class Initialize {
 
     public static String FACEBOOK_PAGE_ID = "EPIC.ADE.CHLEF";
     public static String FACEBOOK_URL = "https://www.facebook.com/" + FACEBOOK_PAGE_ID;
     public static String WEBSITE_URL = "https://www.ade.dz/e-paiement/";
 
-    public static String HOST_NAME = "http://192.168.1.100/login";
+
+    public static String HOST_NAME = "http://192.168.1.10/ade";
 
 }
