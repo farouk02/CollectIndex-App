23d594579dcbb26cb8ed9593ad14c1d28f74108d
farouk02 <faroukious0@gmail.com>
2021-07-09 00:40:09 +0100

    uppp

--- app/src/main/java/com/example/ade/Initialize.java
+++ app/src/main/java/com/example/ade/Initialize.java
@@ -7,7 +7,5 @@ public class Initialize {
     public static String FACEBOOK_URL = "https://www.facebook.com/" + FACEBOOK_PAGE_ID;
     public static String WEBSITE_URL = "https://www.ade.dz/e-paiement/";
 
-
-    public static String HOST_NAME = "http://192.168.1.100/ade";
-
+    public static String HOST_NAME = "http://192.168.43.100/ade";
 }
